d63edb277147624af894e5e556a1f3268058dba6
mounanaji810-star <mounanaji810@gmail.com>
2026-03-11 03:58:24 -0700

    Add files via upload

---
+++ DepartementRepository.java
@@ -0,0 +1,12 @@
+package com.projet.itreclamation.repository;
+
+import com.projet.itreclamation.model.entity.Departement;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartementRepository extends JpaRepository<Departement, Long> {
+
+
+    Departement findByNom(String nom);
+}
